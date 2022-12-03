b52cd3f63ea0b38a8c9567b9eef0a02980198f90
mayurW1000 <mayur.wable1000@gmail.com>
2022-12-03 23:11:08 +0530

    commit

--- src/main/java/com/driver/models/Book.java
+++ src/main/java/com/driver/models/Book.java
@@ -45,5 +45,11 @@ public class Book {
 
     public Book() {
     }
+
+    public Book(String name, Genre genre, Author author) {
+        this.name = name;
+        this.genre=genre;
+        this.author=author;
+    }
 }
 
